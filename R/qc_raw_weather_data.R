58a13dee6d40d570c925b73852b49cff802aa7d6
cjubin <cathy.jubin@uni-goettingen.de>
2022-12-06 21:47:21 +0100

    solar radiation not obligatory to provide

--- R/qc_raw_weather_data.R
+++ R/qc_raw_weather_data.R
@@ -183,7 +183,7 @@ qc_raw_weather_data <-
         dates_to_remove <-
           daily_weather_data[daily_weather_data$IDenv == j, "YYYYMMDD"][daily_weather_data[daily_weather_data$IDenv == j, "YYYYMMDD"] %notin%
                                                                           dates_to_keep]
-        print(dates_to_remove)
+        
         if (length(dates_to_remove) > 0) {
           daily_weather_data <- daily_weather_data %>%
             filter(!(YYYYMMDD %in% dates_to_remove & IDenv %in% j))
